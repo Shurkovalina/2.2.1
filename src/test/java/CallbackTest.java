2d070877f2023583b809da831563e13ca2cc57c8
Shurkovalina <shurkowalina@yandex.ru>
2021-03-09 14:46:40 +0300

    adding changes

--- src/test/java/CallbackTest.java
+++ src/test/java/CallbackTest.java
@@ -1,48 +1,56 @@
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
+
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CallbackTest {
 
+    @BeforeEach
+    void opening() {
+        open("http://localhost:9999");
+    }
+
+    public String date(int shift) {
+        LocalDate date = LocalDate.now();
+        LocalDate returnDate = date.plusDays(shift);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        return formatter.format(returnDate);
+    }
+
     @Test
     void shouldSubmitRequest() {
-        Configuration.timeout = 15000;
-        open("http://localhost:9999");
-        LocalDate date = LocalDate.now();
-        LocalDate returnDate = date.plusDays(9);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.YYYY");
+
         $("[data-test-id=city] input").setValue("Тула");
         $("[placeholder='Дата встречи']").sendKeys(Keys.CONTROL + "a");
         $("[placeholder='Дата встречи']").sendKeys(Keys.BACK_SPACE);
-        $("[placeholder='Дата встречи']").setValue(""+formatter.format(returnDate));
+        $("[placeholder='Дата встречи']").setValue("" + date(9));
         $("[data-test-id=name] input").setValue("Морозов Матвей");
         $("[data-test-id=phone] input").setValue("+79200000000");
         $("[data-test-id=agreement]").click();
         $(withText("Забронировать")).click();
-        $(withText("Успешно!")).shouldBe(Condition.visible);
+        $(withText("Успешно!")).shouldBe(Condition.visible, Duration.ofMillis(15000));
+        $(withText(""+date(9))).shouldBe(Condition.visible);
     }
 
     @Test
     void shouldSubmitRequest2() {
-        Configuration.timeout = 15000;
-        open("http://localhost:9999");
-        LocalDate date = LocalDate.now();
-        LocalDate returnDate = date.plusDays(9);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.YYYY");
         $("[data-test-id=city] input").setValue("Ту");
         $(withText("Тула")).click();
         $("[placeholder='Дата встречи']").sendKeys(Keys.CONTROL + "a");
         $("[placeholder='Дата встречи']").sendKeys(Keys.BACK_SPACE);
-        $("[placeholder='Дата встречи']").setValue(""+formatter.format(returnDate));
+        $("[placeholder='Дата встречи']").setValue("" + date(9));
         $("[data-test-id=name] input").setValue("Морозов Матвей");
         $("[data-test-id=phone] input").setValue("+79200000000");
         $("[data-test-id=agreement]").click();
         $(withText("Забронировать")).click();
-        $(withText("Успешно!")).shouldBe(Condition.visible);
+        $(withText("Успешно!")).shouldBe(Condition.visible, Duration.ofMillis(15000));
+        $(withText(""+date(9))).shouldBe(Condition.visible);
     }
 }
